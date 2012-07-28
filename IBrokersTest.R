6f495a86caffaace4ea72608d263bc8a113bee0c
lalas <ah.mednagi@gmail.com>
2012-07-28 18:38:18 -0400

    TestingHowToCommit

---
+++ IBrokersTest.R
@@ -0,0 +1,16 @@
+library(IBrokers)
+tws <- twsConnect()
+contract.test <- twsEquity('AAPL')
+apple.test <- reqHistoricalData(tws,
+                  contract.test,
+                  barSize = "30 secs",
+                  duration = "1 D",
+                  useRTH = "1",
+                  whatToShow = "BID_ASK",
+                  timeFormat = "1",
+                  tzone = "",
+                  verbose = TRUE,
+                  tickerId = "1")
+
+Sys.sleep(10) # mandatory 10s between request to avoid IB pacing violation see the following link for more info
+#http://www.interactivebrokers.com/php/apiUsersGuide/apiguide/api/historical_data_limitations.htm
