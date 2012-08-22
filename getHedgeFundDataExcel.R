1d3bde97534110d3ca6a044f93ca2d9a1ca243e6
lalas <ah.mednagi@gmail.com>
2012-08-22 01:09:21 -0400

    Added a function to get a number of common tickers to analyze

--- getHedgeFundDataExcel.R
+++ getHedgeFundDataExcel.R
@@ -10,7 +10,7 @@ getHedgeFund.ExcelData <- function(workbook = 'Hedge Fund Managers of interest.x
   for (i in 1:n){
     if (sheets.name[i] != 'Comments') {
       n2 <- n2 + 1
-      data <- readWorksheet(wb, sheet = sheets.name[i], startRow = 3, startCol = 2)
+      data <- readWorksheet(wb, sheet = sheets.name[i], startRow = 4, startCol = 2)
       tmp <- strsplit(sheets.name[i], " ")
       port.manager <- tmp[[1]][1]
       rebalance.date <- as.Date(as.yearqtr(tmp[[1]][2], "%Y-%m"), frac = 1) # converting 2nd part of sheet name to date
@@ -21,5 +21,32 @@ getHedgeFund.ExcelData <- function(workbook = 'Hedge Fund Managers of interest.x
   
   return(Port.Cont)
 }
+getTop <- function(p = 0.1, portfolio.data) {
+  ## usefull function to could have been used instead of missing/in conjuction with missing below
+  ## http://r.789695.n4.nabble.com/get-arguments-passed-to-function-inside-a-function-td3783894.html
+  # t <- as.list(match.call())
+  # a <- which(names(t) == 'portfolio.data')
+  # if(length(a)) { print(we have it)}
+  # else { print("we don't have a second argument")}
+  #
+  ## another way is to save the enviroment variable in a list, and check this list:
+  # t <- as.list(environment())
+  # which(names(t) == 'portfolio.data')
+  if (missing(portfolio.data)){
+    portfolio.data <- getHedgeFund.ExcelData()
+    print("Getting portfolio data")
+  }
 
-
+  x1 <- ldply(portfolio.data, function(x) x[["Data"]][c("SECURITY","TICKER","X..PORT")])
+  x2 <- count(x1, c("SECURITY","TICKER"),"X..PORT")
+  x3 <- na.omit(arrange(x2,desc(freq))) ## OR x3 <- na.omit(x2[order(x2$freq,decreasing=T),])
+  x3$p <- x3$freq/sum(x3$freq)
+  x3$P <- cumsum(x3$p)
+  if (p < 1) indx <- which(x3$P < (1-p))
+  else {
+    #warning("MyWarn: p has to be less than 1")
+    indx <- 1:length(x3$freq)
+  }
+  #return(x3[indx,])
+  return(t)
+}
