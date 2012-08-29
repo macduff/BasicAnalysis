e5b45d52d6ca68885db2e46ee8adcb0bd239b091
lalas <ah.mednagi@gmail.com>
2012-08-28 23:54:22 -0400

    made getTop more generic

--- getHedgeFundDataExcel.R
+++ getHedgeFundDataExcel.R
@@ -21,17 +21,18 @@ getHedgeFund.ExcelData <- function(workbook = 'Hedge Fund Managers of interest.x
   
   return(Port.Cont)
 }
-getTop <- function(p = 0.1, portfolio.data) {
+getTop <- function(N = 10, portfolio.data, reb.date) {
+  require(plyr)
   ## usefull function to could have been used instead of missing/in conjuction with missing below
   ## http://r.789695.n4.nabble.com/get-arguments-passed-to-function-inside-a-function-td3783894.html
   # t <- as.list(match.call())
   # a <- which(names(t) == 'portfolio.data')
   # if(length(a)) { print(we have it)}
-  # else { print("we don't have a second argument")}
+  # else { print("we don't have a second argument")
   #
   ## another way is to save the enviroment variable in a list, and check this list:
   # t <- as.list(environment())
-  # which(names(t) == 'portfolio.data')
+  # which(names(t) == 'portfolio.data')}
   if (missing(portfolio.data)){
     portfolio.data <- getHedgeFund.ExcelData()
     print("Getting portfolio data")
@@ -42,11 +43,24 @@ getTop <- function(p = 0.1, portfolio.data) {
   x3 <- na.omit(arrange(x2,desc(freq))) ## OR x3 <- na.omit(x2[order(x2$freq,decreasing=T),])
   x3$p <- x3$freq/sum(x3$freq)
   x3$P <- cumsum(x3$p)
-  if (p < 1) indx <- which(x3$P < (1-p))
-  else {
-    #warning("MyWarn: p has to be less than 1")
-    indx <- 1:length(x3$freq)
-  }
-  #return(x3[indx,])
-  return(t)
+  ## Different approache to keep the Date information
+  x4 <- ldply(portfolio.data, function(x) cbind(x[["Data"]][c("SECURITY","TICKER","X..PORT")], x[["Rebalancing.date"]]))
+  names(x4)[4] <- "Rebalancing.Date"
+  x5 <- count(na.omit(x4), c("SECURITY","TICKER", "Rebalancing.Date") ,"X..PORT")
+  x6 <- arrange(x5,desc(Rebalancing.Date), desc(freq))
+  if (missing(reb.date)) reb.date <- unique(x4$Rebalancing.Date)
+  for (j in 1:length(reb.date)) {
+    if (j == 1) {
+      if (N > 1)
+        res <- x4[x4$Rebalancing.Date==reb.date[j],][1:N,]
+      else res <- x4[x4$Rebalancing.Date==reb.date[j],]
+      }
+    else {
+      if (N > 1)
+        res <- rbind(res, x4[x4$Rebalancing.Date==reb.date[j],][1:N,])
+      else res <- rbind(res,x4[x4$Rebalancing.Date==reb.date[j],])
+      }
+    }
+  return(res)
+  
 }
